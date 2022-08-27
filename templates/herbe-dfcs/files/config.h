bfe478df2dab04908f277726e0a498c322a36cef
diego castillo salazar <d13g0x>
2022-08-27 07:45:04 -0500

    First update

---
+++ templates/herbe-dfcs/files/config.h
@@ -0,0 +1,19 @@
+static const char *background_color = "#101010";
+static const char *border_color = "#252525";
+static const char *font_color = "#999999";
+static const char *font_pattern = "Hack Nerd Font:size=8";
+static const unsigned line_spacing = 5;
+static const unsigned int padding = 10;
+
+static const unsigned int width = 300;
+static const unsigned int border_size = 2;
+static const unsigned int pos_x = 20;
+static const unsigned int pos_y = 30;
+
+enum corners { TOP_LEFT, TOP_RIGHT, BOTTOM_LEFT, BOTTOM_RIGHT };
+enum corners corner = TOP_RIGHT;
+
+static const unsigned int duration = 5; /* in seconds */
+
+#define DISMISS_BUTTON Button1
+#define ACTION_BUTTON Button3
